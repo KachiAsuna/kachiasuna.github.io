9390923527413946ab9ccfa82f53e95bb44522f2
KachiAsuna <123526769+KachiAsuna@users.noreply.github.com>
2024-01-01 14:01:57 +0700

    Update script.js

--- profile/script.js
+++ profile/script.js
@@ -329,26 +329,3 @@ function updateVisitorCount() {
 // Gọi hàm cập nhật số lượng người vào website khi trang được tải
 updateVisitorCount();
 
-var countDownDate = new Date("January 1, 2024 00:00:00 GMT+0700").getTime();
-
-var x = setInterval(function () {
-    var now = new Date().getTime();
-    var distance = countDownDate - now;
-
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-    // Hàm để thêm số 0 đằng trước nếu giá trị nhỏ hơn 10
-    function addLeadingZero(value) {
-        return value < 10 ? "0" + value : value;
-    }
-
-    // Gán giá trị cho các phần tử HTML
-    document.getElementById("days").innerHTML = addLeadingZero(days);
-    document.getElementById("hours").innerHTML = addLeadingZero(hours);
-    document.getElementById("minutes").innerHTML = addLeadingZero(minutes);
-    document.getElementById("seconds").innerHTML = addLeadingZero(seconds);
-
-}, 1000);
